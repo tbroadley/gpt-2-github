856406062526ef77f1f6ead6058707f76d1dbed5
Thomas Broadley <buriedunderbooks@hotmail.com>
2020-05-17 16:02:50 -0400

    Filter HTML, links, inline code with spaces

--- clean-markdown.js
+++ clean-markdown.js
@@ -1,4 +1,18 @@
 const filter = require("unist-util-filter");
 
-module.exports = () => (tree, file) =>
-  filter(tree, { cascade: false }, (node) => node.type !== "code");
+module.exports = () => (tree, file) => {
+  return filter(tree, { cascade: false }, (node) => {
+    switch (node.type) {
+      case "code":
+      case "html":
+      case "link":
+        return false;
+      case "inlineCode":
+        return !node.value.match(/\s/);
+      case "text":
+        return !node.value.match(/https?:\/\//);
+      default:
+        return true;
+    }
+  });
+};
